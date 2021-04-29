cb9b84d1fedea0c259227cf15824ed1e6ec6ab16
Despina Paliou <despoina.paliou@gmail.com>
2021-04-29 16:23:52 +0300

    ssh key for connecting in jenkins ec2

--- lab-network/instances.tf
+++ lab-network/instances.tf
@@ -9,3 +9,17 @@ data "aws_ssm_parameter" "linuxAmiParis" {
   provider = aws.region-worker
   name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
+
+#Create key-pair for logging into EC2 in eu-west-2
+resource "aws_key_pair" "master-key" {
+  provider   = aws.region-master
+  key_name   = "jenkins"
+  public_key = file("~/code/id_rsa_aws.pub")
+}
+
+#Create key-pair for logging into EC2 in eu-west-3
+resource "aws_key_pair" "worker-key" {
+  provider   = aws.region-worker
+  key_name   = "jenkins"
+  public_key = file("~/code/id_rsa_aws.pub")
+}
