a425880458da9bb5352df26b014a1c4cd29ac9b5
Despina Paliou <despoina.paliou@gmail.com>
2021-04-29 16:20:03 +0300

    linux AMI retrieving

--- lab-network/README.md
+++ lab-network/README.md
@@ -2,3 +2,6 @@
 
 <h3>Create S3 bucket</h3>
 $ aws s3api create-bucket --bucket dpaliouterrabucket --create-bucket-configuration LocationConstraint=eu-west-2
+
+<h3>Retrieve terraformstatefile (json)</h3>
+$ aws s3 cp s3://dpaliouterrabucket/terraformstatefile .
---
+++ lab-network/instances.tf
@@ -0,0 +1,11 @@
+#Get Linux AMI ID using SSM Parameter endpoint in eu-west-2
+data "aws_ssm_parameter" "linuxAmi" {
+  provider = aws.region-master
+  name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+#Get Linux AMI ID using SSM Parameter endpoint in eu-west-3
+data "aws_ssm_parameter" "linuxAmiParis" {
+  provider = aws.region-worker
+  name     = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
