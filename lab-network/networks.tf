0fa55c5733b4b5188fe1b3fe4b48458d9d5b4bf7
Despina Paliou <despoina.paliou@gmail.com>
2021-04-29 12:52:38 +0300

    add routing tables - the VPCs communicate

--- lab-network/networks.tf
+++ lab-network/networks.tf
@@ -59,3 +59,79 @@ resource "aws_subnet" "subnet_1_paris" {
   cidr_block = "192.168.1.0/24"
 }
 
+
+##########################################################################################
+##########################################################################################
+##########################################################################################
+
+###########################
+#Initiate Peering connection request from eu-west-2
+resource "aws_vpc_peering_connection" "euwest2-euwest3" {
+  provider    = aws.region-master
+  peer_vpc_id = aws_vpc.vpc_master_paris.id
+  vpc_id      = aws_vpc.vpc_master.id
+  peer_region = var.region-worker
+}
+#Accept VPC peering request in eu-west-3 from eu-west-2
+resource "aws_vpc_peering_connection_accepter" "accept_peering" {
+  provider                  = aws.region-worker
+  vpc_peering_connection_id = aws_vpc_peering_connection.euwest2-euwest3.id
+  auto_accept               = true
+}
+
+#Create route table in eu-west-2 - The VPCs be able to communicate
+resource "aws_route_table" "internet_route" {
+  provider = aws.region-master
+  vpc_id   = aws_vpc.vpc_master.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  route {
+    cidr_block                = "192.168.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.euwest2-euwest3.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Master-Region-RT"
+  }
+}
+
+#overwrite default route table of VPC(master) with our route table entries
+#because when we created the VPC above, it created the default route over VPC, so we update the main routing table
+resource "aws_main_route_table_association" "set-master-default-rt-assoc" {
+  provider       = aws.region-master
+  route_table_id = aws_route_table.internet_route.id
+  vpc_id         = aws_vpc.vpc_master.id
+}
+
+###########################
+#Create route table in eu-west-3
+resource "aws_route_table" "internet_route_paris" {
+  provider = aws.region-worker
+  vpc_id   = aws_vpc.vpc_master_paris.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw-paris.id
+  }
+  route {
+    cidr_block                = "10.0.1.0/24"
+    vpc_peering_connection_id = aws_vpc_peering_connection.euwest2-euwest3.id
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+  tags = {
+    Name = "Worker-Region-RT"
+  }
+}
+
+#overwrite default route table of VPC(worker) with our route table entries
+resource "aws_main_route_table_association" "set-worker-default-rt-assoc" {
+  provider       = aws.region-worker
+  route_table_id = aws_route_table.internet_route_paris.id
+  vpc_id         = aws_vpc.vpc_master_paris.id
+}
+
